67e8b8892e124f33c9d57407ca117a9acd3e9fd9
LeniTD <lenitd603@gmail.com>
2021-06-07 08:20:17 -0500

    v2

--- js/script.js
+++ js/script.js
@@ -7,13 +7,40 @@ function mostrar_menu(){
 }
 
 
+// Scroll up
+
+document.getElementById("button-up").addEventListener("click", scrollUp);
+
+function scrollUp(){
+
+    var currentScroll = document.documentElement.scrollTop;
+
+    if (currentScroll > 0){
+        window.requestAnimationFrame(scrollUp);
+        window.scrollTo (0, currentScroll - (currentScroll / 10));
+    }
+}
 
 
 
+///
+
+buttonUp = document.getElementById("button-up");
+
+window.onscroll = function(){
+
+    var scroll = document.documentElement.scrollTop;
+
+    if (scroll > 500){
+        buttonUp.style.transform = "scale(1)";
+    }else if(scroll < 500){
+        buttonUp.style.transform = "scale(0)";
+    }
+
+}
 
 
-
-                            //Buscador de contenido
+//Buscador de contenido
 
 
 //Ejecutando funciones
